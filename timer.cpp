56034dacd7246b3d3f1e7e17a962298f74df8191
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-12-02 16:47:24 +0100

    Create timer.cpp

---
+++ timer.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+string options(){
+    string a = "";
+    string b = "";
+    cout << "            __________________________________________               " << endl;
+    cout << "           /__   ____________________________________/               " << endl;
+    cout << "             /  / __   _________   ______   ________                 " << endl;
+    cout << "            /  / |  | |  _   _  | | =====| |  ----_/                 " << endl;
+    cout << "           /__/  |__| |_| |_| |_| |______| |_|  \\_\\                " << endl;
+    cout << "*******************************************************************\n" << endl;
+    return a, b;
+};
+
+int main()
+{
+    string hello = "";
+    string g = "";
+    hello, g = options();
+    cout << g << endl;
+
+    return 0;
+}
