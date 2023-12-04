0937beeedc68f2a99e7a61dff0e67b1ea5f322d4
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-12-04 18:46:59 +0100

    Update timer.cpp

--- timer.cpp
+++ timer.cpp
@@ -3,23 +3,31 @@
 using namespace std;
 
 string options(){
-    string a;
-    string b;
+    string optionsOne;
     cout << "            __________________________________________               " << endl;
     cout << "           /__   ____________________________________/               " << endl;
     cout << "             /  / __   _________   ______   ________                 " << endl;
     cout << "            /  / |  | |  _   _  | | =====| |  ----_/                 " << endl;
     cout << "           /__/  |__| |_| |_| |_| |______| |_|  \\_\\                " << endl;
     cout << "*******************************************************************\n" << endl;
-    return a, b;
+
+    cout << "-------------------------ESCOJA UNA OPCIÓN-------------------------" << endl;
+    cout << "A) Calcular numero de dias entre una fecha y la actual." <<endl;
+    cout << "B) Calcular numero de dias entre dos fechas." <<endl;
+    cout << "C) Conocer fecha a partir del numero de dias." <<endl;
+    cout << "-------------------------------------------------------------------\n" << endl;
+
+    cout << "Introduzca aqui su opcion: "; cin >> optionsOne;
+
+    return optionsOne;
 };
 
 int main()
 {
-    string hello;
-    string g;
-    hello, g = options();
-    cout << g << endl;
+
+    string option;
+    option = options();
+    cout << "Ha escojido la opcion " << option << endl;
 
     return 0;
 }
