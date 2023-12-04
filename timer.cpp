eb1daed9bfc3bd9ffba1bb475d9b5049a0f4e296
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-12-04 22:00:46 +0100

    Update timer.cpp

--- timer.cpp
+++ timer.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
@@ -10,12 +11,19 @@ void LimpiarPantalla(){
 
 string options(){
     string optionsOne;
+    time_t tiempo_actual = std::time(nullptr);
+    tm* tiempo_descompuesto = std::localtime(&tiempo_actual);
+
     cout << "            __________________________________________               " << endl;
     cout << "           /__   ____________________________________/               " << endl;
     cout << "             /  / __   _________   ______   ________                 " << endl;
     cout << "            /  / |  | |  _   _  | | =====| |  ----_/                 " << endl;
     cout << "           /__/  |__| |_| |_| |_| |______| |_|  \\_\\                " << endl;
     cout << "*******************************************************************\n" << endl;
+    cout << "                             "
+              << tiempo_descompuesto->tm_mday << '/'
+              << tiempo_descompuesto->tm_mon + 1 << '/'
+              << tiempo_descompuesto->tm_year + 1900 << endl;
 
     cout << "-------------------------ESCOJA UNA OPCION-------------------------" << endl;
     cout << "A) Calcular numero de dias entre una fecha y la actual." <<endl;
