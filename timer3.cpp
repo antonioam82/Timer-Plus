24acb7ad55cc9e83cc1602ca3a7321669ef4d1dc
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2024-11-10 20:08:43 +0100

    Create timer3.cpp

---
+++ timer3.cpp
@@ -0,0 +1,243 @@
+#include <iostream>
+#include <ctime>
+#include <locale>
+#include <limits>
+#include <sstream>
+#include <cctype>
+#include <chrono>
+#include "date/date.h"
+
+using namespace std;
+
+void LimpiarPantalla(){
+    #ifdef _WIN32
+        system("cls"); // Windows
+    #else
+        system("clear"); // Linux / MacOS
+    #endif
+}
+
+
+int obtenerNumeroEntero(string mensaje, int minVal, int maxVal) {
+    int numero;
+    string input;
+
+    while (true) {
+        cout << mensaje;
+        cin >> input;
+
+        stringstream ss(input);
+        if (ss >> numero && ss.eof() && numero >= minVal && numero <= maxVal) {
+            break;
+        }
+
+        cout << "Por favor, ingrese un numero valido en el rango (" << minVal << " - " << maxVal << ")." << endl;
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    return numero;
+}
+
+int getCurrentDate(){
+    // Obtiene el tiempo actual en formato de tiempo de UNIX
+    std::time_t tiempo_actual = std::time(nullptr);
+
+    // Convierte el tiempo a una estructura tm
+    std::tm* tiempo_descompuesto = std::localtime(&tiempo_actual);
+
+    // Muestra la fecha actual
+    std::cout << "                             "
+              << tiempo_descompuesto->tm_mday << '/'
+              << tiempo_descompuesto->tm_mon + 1 << '/'
+              << tiempo_descompuesto->tm_year + 1900 << std::endl;
+
+    return 0;
+
+}
+
+bool bisiesto(int year){
+    if((year % 100 == 0 && year % 400 == 0) | (year % 4 == 0 && year % 100 != 0)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+int checkDays(int year, int month){
+    int maxDay;
+    bool esBisiesto;
+
+    if(month == 2){
+        esBisiesto = bisiesto(year);
+        if(esBisiesto){
+            //cout << "Es bisisesto" << endl;
+            maxDay = 29;
+        }else{
+            //cout << "No es bisisesto" << endl;
+            maxDay = 28;
+        }
+    }else{ // 7 8 10 12
+        if(month == 1 | month == 3 | month == 5 | month == 7 | month == 8 | month == 10 | month == 12){
+            maxDay = 31;
+        }else{
+            maxDay = 30;
+        }
+    }
+
+    return maxDay;
+}
+
+int daysDiff0() {
+    tm date1 = { 0 }; // Inicializa la estructura de fecha
+    int maxDay;
+
+    //cout << "\nIntroduzca anno del suceso: "; cin >> yearEntry1;
+    int yearEntry1 = obtenerNumeroEntero("\nIntroduzca anno del suceso: ", INT_MIN, INT_MAX);
+    int monthEntry1 = obtenerNumeroEntero("Introduzca mes del suceso (1-12): ", 1, 12);/////////////////////////////////
+    maxDay = checkDays(yearEntry1, monthEntry1);
+    int dayEntry1 = obtenerNumeroEntero("Introduzca dia del suceso: ", 1, maxDay);///////////////////////////////////////////////
+
+    date1.tm_year = yearEntry1 - 1900;
+    date1.tm_mon = monthEntry1 - 1;
+    date1.tm_mday = dayEntry1;
+
+    time_t tiempo_actual = time(nullptr);
+    tm* tiempo_descompuesto = localtime(&tiempo_actual);
+
+    time_t time1 = mktime(&date1);
+    time_t time2 = mktime(tiempo_descompuesto);
+
+    double diferencia_segundos = difftime(time2, time1);
+    double dias = diferencia_segundos / (60 * 60 * 24);
+    int weeks = dias / 7;
+    int resto_dias = static_cast<int>(dias) % 7;
+    cout << "\nDiferencia en dias: " << static_cast<int>(dias) << " dias";
+    cout << "\n" << weeks << " semanas" << " y " << resto_dias << " dias" << endl;
+    return 0;
+}
+
+int daysDiff(){
+    tm date1 = {0}; // Inicializa las estructuras de fechas
+    tm date2 = {0};
+    int maxDay;
+    //int val1, val2 = checkDays();
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+    int yearEntry1 = obtenerNumeroEntero("\nIntroduzca anno del primer suceso: ", INT_MIN, INT_MAX);
+    int monthEntry1 = obtenerNumeroEntero("Introduzca mes del primer suceso (1-12): ", 1, 12);
+    maxDay = checkDays(yearEntry1, monthEntry1);
+    int dayEntry1 = obtenerNumeroEntero("Introduzca dia del primer suceso: ", 1, maxDay);
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    int yearEntry2 = obtenerNumeroEntero("\nIntroduzca anno del segundo suceso: ", INT_MIN, INT_MAX);
+    int monthEntry2 = obtenerNumeroEntero("Introduzca mes del segundo suceso (1-12): ", 1, 12);
+    maxDay = checkDays(yearEntry2, monthEntry2);
+    int dayEntry2 = obtenerNumeroEntero("Introduzca dia del segundo suceso: ", 1, maxDay);
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    date1.tm_year = yearEntry1 - 1900;
+    date1.tm_mon = monthEntry1 - 1;
+    date1.tm_mday = dayEntry1;
+
+    date2.tm_year = yearEntry2 - 1900;
+    date2.tm_mon = monthEntry2 - 1;
+    date2.tm_mday = dayEntry2;
+
+    time_t time1 = mktime(&date1);
+    time_t time2 = mktime(&date2);
+
+    double diferencia_segundos = difftime(time2, time1);
+    double dias = diferencia_segundos / (60 * 60 * 24);
+    int weeks = dias / 7;
+    int resto_dias = static_cast<int>(dias) % 7;
+    cout << "\nDiferencia en dias: " << static_cast<int>(dias) << " dias" << endl;
+    cout << "\n" << weeks << " semanas" << " y " << resto_dias << " dias" << endl;
+
+    return 0;
+
+}
+
+void dateFromNum(){
+    //int x;
+    int numeroDias;
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+
+    cout << "Introduce numero de dias: "; cin >> numeroDias;
+    time_t dateBeforeXDays = now + (numeroDias * 24 * 3600);
+    tm *ltmBeforeXDays = localtime(&dateBeforeXDays);
+
+    if(numeroDias > 0){
+        cout << "Fecha dentro de " << numeroDias << " dias: " << ltmBeforeXDays->tm_mday << "/" << 1 + ltmBeforeXDays->tm_mon << "/" << 1900 + ltmBeforeXDays->tm_year << endl;
+    }
+    else if(numeroDias < 0){
+        cout << "Fecha hace " << abs(numeroDias) << " dias: " << ltmBeforeXDays->tm_mday << "/" << 1 + ltmBeforeXDays->tm_mon << "/" << 1900 + ltmBeforeXDays->tm_year << endl;
+    }
+    else{
+        cout << "Hoy es: " << ltm->tm_mday << "/" << 1 + ltm->tm_mon << "/" << 1900 + ltm->tm_year << endl;
+    }
+
+}
+
+string options(){
+    string optionsOne;
+    LimpiarPantalla();
+    //string frase = "";
+    //int longitud = int(frase.length());
+    //cout << "Longitud frase: " << longitud << endl;
+
+    cout << "            __________________________________________               " << endl;
+    cout << "           /__   ____________________________________/               " << endl;
+    cout << "             /  / __   _________   ______   ________                 " << endl;
+    cout << "            /  / |  | |  _   _  | | =====| |  ----_/                 " << endl;
+    cout << "           /__/  |__| |_| |_| |_| |______| |_|  \\_\\                " << endl;
+    cout << "*******************************************************************\n" << endl;
+    getCurrentDate();
+
+    cout << "-------------------------ESCOJA UNA OPCION-------------------------" << endl;
+    cout << "A) Calcular numero de dias entre una fecha y la actual." <<endl;
+    cout << "B) Calcular numero de dias entre dos fechas." <<endl;
+    cout << "C) Conocer fecha a partir del numero de dias." <<endl;
+    cout << "D) Salir del programa." << endl;
+    cout << "-------------------------------------------------------------------\n" << endl;
+
+    cout << "Introduzca aqui su opcion: "; cin >> optionsOne;
+
+    //optionsOne = toupper(optionsOne[0]); //conversion a mayuscula
+    if (!optionsOne.empty()) {
+        optionsOne[0] = toupper(optionsOne[0]);
+    }
+
+    return optionsOne;
+};
+
+int main()
+{
+    locale::global(std::locale(""));
+    string option;
+    string cont;
+    while(option != "D"){
+        option = options();
+
+        while(option != "A" && option != "B" && option != "C" && option != "D"){
+            cout << "\nOPCION NO VALIDA: " << option << endl;
+            cout << "\nINGRESE CUALQUIER TECLA PARA CONTINUAR: "; cin >> cont;
+            option = options();
+        }
+        if(option == "A"){
+            daysDiff0();
+        }
+        if(option == "B"){
+            daysDiff();
+        }
+        if(option == "C"){
+            dateFromNum();
+        }
+        if(option != "D"){
+            cout << "\nINGRESE CUALQUIER TECLA PARA CONTINUAR: "; cin >> cont;
+        }
+    }
+    LimpiarPantalla();
+    return 0;
+}
