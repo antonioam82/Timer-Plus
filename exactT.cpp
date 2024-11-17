b6adf88ec74e5d18e095abf0f34622126cb3f9f8
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2024-11-17 20:02:15 +0100

    Update exactT.cpp

--- exactT.cpp
+++ exactT.cpp
@@ -51,14 +51,61 @@ int numeroEntero(string mensaje) {
     return numero;
 }
 
+// Función que obtiene la fecha actual y la devuelve por referencia
+void currentDate(int &year, int &month, int &day) {
+    // Obtener el tiempo actual en formato de tiempo del sistema
+    auto now = std::chrono::system_clock::now();
+
+    // Convertir el tiempo a un objeto de tipo time_t
+    std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+
+    // Convertir el time_t a una estructura tm (tiempo local)
+    std::tm* now_tm = std::localtime(&now_time);
+
+    // Asignar el año, mes y día por separado
+    year = now_tm->tm_year + 1900;   // tm_year es el número de años desde 1900
+    month = now_tm->tm_mon + 1;      // tm_mon es el número de meses desde enero (0-11)
+    day = now_tm->tm_mday;           // tm_mday es el día del mes (1-31)
+
+    // Imprimir los valores obtenidos (opcional)
+    cout << "Fecha actual -> Año: " << year << " Mes: " << month << " Día: " << day << endl;
+}
+
+
+int validateNum(string text, int minNum, int maxNum){
+    int numero;
+    string entrada;
+
+    while (true){
+        cout << text;
+        cin >> entrada;
+
+        stringstream ss(entrada);
+        if (ss >> numero && ss.eof() && numero >= minNum && numero <= maxNum){
+            break;
+        }
+        cout << "Por favor, ingrese un numero valido en el rango (" << minNum << " - " << maxNum << ")." << endl;
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    }
+    return numero;
+
+}
+
 int main()
 {
     char exit_program = 'n';
+    int currentYear, currentMonth, currentDay;
+
+    // Obtener la fecha actual
+    currentDate(currentYear, currentMonth, currentDay);
+
     while(exit_program == 'n'){
-        int year = numeroEntero("Introduce anno de nacimiento: ");
-        int month = numeroEntero("Introduce mes de nacimiento: ");
-        int day = numeroEntero("Introduce dia de nacimiento: ");
-        cout << "NACISTE EL " << day << " DE " << month << " DE " << year << endl;
+        int year = validateNum("Introduce anno de nacimiento: ",1,currentYear);
+        int month = validateNum("Introduce mes de nacimiento: ",1,12);
+        int day = validateNum("Introduce dia de nacimiento: ",1,31);
+        cout << "NACISTE EL " << day << " DEL " << month << " DE " << year << endl;
         exit_program = question();
         LimpiarPantalla();
     }
